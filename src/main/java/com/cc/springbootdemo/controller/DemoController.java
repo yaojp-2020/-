32dec082287c78e9831f09b8f4cca7de080777a4
yaojp-2000 <1660795239@qq.com>
2020-04-19 16:10:44 +0800

    控制层改动

--- src/main/java/com/cc/springbootdemo/controller/DemoController.java
+++ src/main/java/com/cc/springbootdemo/controller/DemoController.java
@@ -4,5 +4,6 @@ public class DemoController {
 
     public static void main(String[] args) {
         System.out.println("helloWorld!!!!!!!");
+        System.out.println("cc world!!!!!!!!!");
     }
 }

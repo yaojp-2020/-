21dc4c8523b8d32e0d38f27cc7d0a6c15a6e2b66
yaojp-2000 <1660795239@qq.com>
2020-04-19 15:46:52 +0800

    测试文件

---
+++ src/main/java/com/cc/springbootdemo/controller/DemoController.java
@@ -0,0 +1,8 @@
+package com.cc.springbootdemo.controller;
+
+public class DemoController {
+
+    public static void main(String[] args) {
+        System.out.println("helloWorld!!!!!!!");
+    }
+}
